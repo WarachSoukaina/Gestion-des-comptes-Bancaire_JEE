df08564a054e79266421cd8d35dae7b7c1835b53
ws soukaina <w_soukaina@hotmail.fr>
2019-05-19 16:34:23 +0100

    commit2

--- src/main/java/org/warach/web/BanqueController.java
+++ src/main/java/org/warach/web/BanqueController.java
@@ -1,8 +1,12 @@
 package org.warach.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.warach.entities.Compte;
+import org.warach.entities.Operation;
 import org.warach.metier.IBanqueMetier;
 
 @Controller
@@ -16,5 +20,25 @@ public class BanqueController {
 		return "comptes";
 		
 	}
+	
+	@RequestMapping("/consulterCompte")
+	public String consulter(Model model, String codeCompte) {
+		model.addAttribute("codeCompte", codeCompte);
+		
+		try {
+			Compte cp = banqueMetier.consuleterCompte(codeCompte);
+			Page<Operation> pageOperations = banqueMetier.listOperation(codeCompte, 0, 5);
+			model.addAttribute("compte", cp);
+			model.addAttribute("listOperations",pageOperations.getContent());
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+			model.addAttribute("exception", e);
+		}
+		
+		return "comptes";
+		
+	}
+	
 
 }
